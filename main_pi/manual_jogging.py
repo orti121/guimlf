13f5104c51dcac0d9ed1ff6890526b00529e375e
orti121 <cristian.ortizdezarate@ug.uchile.cl>
2022-04-28 17:36:13 -0400

    Add files via upload

--- main_pi/manual_jogging.py
+++ main_pi/manual_jogging.py
@@ -8,9 +8,9 @@ from mk2robot import MK2Robot
 import time
 
 robot = MK2Robot(link_lengths=[55, 39, 135, 147, 66.3])
-robot_serial = SerialControl()
+#robot_serial = SerialControl()
 # robot_serial = SerialControl("COM5") #for pc
-robot_serial.open_serial()
+#robot_serial.open_serial()
 
 
 
@@ -24,9 +24,9 @@ try:
             Yval = int(input("Y: "))
             Zval = int(input("Z: "))
             q0, q1, q2 = robot.inverse_kinematics(Xval, Yval, Zval)
-            robot_serial.write_servo(1, 45 + q0)
-            robot_serial.write_servo(2, 90 - q1)
-            robot_serial.write_servo(3, q2 + q1)
+            #robot_serial.write_servo(1, 45 + q0)
+            #robot_serial.write_servo(2, 90 - q1)
+            #robot_serial.write_servo(3, q2 + q1)
             print(f"q0 = {q0}")
             print(f"q1 = {90-q1}")
             print(f"q2 = {q1+q2}")
@@ -36,17 +36,18 @@ try:
             q0val = int(input("q0: "))
             q1val = int(input("q1: "))
             q2val = int(input("q2: "))
-            robot_serial.write_servo(1, q0val)
-            robot_serial.write_servo(2, q1val)
-            robot_serial.write_servo(3, q2val)
+            #robot_serial.write_servo(1, q0val)
+            #robot_serial.write_servo(2, q1val)
+            #robot_serial.write_servo(3, q2val)
         elif selector == "home":
-            robot_serial.write_servo(1, 45)
-            robot_serial.write_servo(2, 90)
-            robot_serial.write_servo(3, 90)
+            #robot_serial.write_servo(1, 45)
+            #robot_serial.write_servo(2, 90)
+            #robot_serial.write_servo(3, 90)
+            pass
         else:
             pass
 
 
 except KeyboardInterrupt:
-    robot_serial.close_serial()
+    #robot_serial.close_serial()
     pass
